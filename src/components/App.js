63a3d14d65fb456a8ec61ecec82af4b616d6432c
Ashay Nayak <ashaynayak@microsoft.com>
2022-08-13 16:09:03 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import Header from './Header';
+import ContactList from './ContactList';
+import AddContact from './AddContact';
+import { v4 as uuid } from 'uuid';
+
+function App() {
+  const LOCAL_STORGE_KEY = "CONTACTS";
+  // to avoid losing data on refresh
+  let temp = [];
+  if(JSON.parse(localStorage.getItem(LOCAL_STORGE_KEY))!==null)temp=JSON.parse(localStorage.getItem(LOCAL_STORGE_KEY));
+  const [contacts,setContacts] = useState(temp);
+
+  const addContactHandler = (contact) => {
+      setContacts([...contacts, { id: uuid(), ...contact }]); // ...contacts -> as we need previous values of array as well.
+        console.log("contacts");
+        console.log(contacts);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+        return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  } 
+
+  // the below useEffect is not setting the contacts value as we set UseState([]) but now it is fixed and we dont need nelow code.
+  // useEffect( () => {
+  //   console.log(localStorage.getItem(LOCAL_STORGE_KEY));
+
+  //   console.log("1");
+  //   const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORGE_KEY));
+  //   console.log(retrieveContacts);
+  //   if(retrieveContacts) setContacts(retrieveContacts);
+  //   console.log(contacts);
+  //   // eslint-disable-next-line
+  // },[]); // [] -> run only once
+
+  useEffect( () => {
+    console.log("HI");
+    localStorage.setItem(LOCAL_STORGE_KEY,JSON.stringify(contacts));
+  }, [contacts]); // runs every time when contacts value changes
+
+  return (
+    <div className="ui container">
+       <React.StrictMode><Header /></React.StrictMode>
+       <AddContact addContactHandler={addContactHandler} />
+       <ContactList contacts={contacts} getContactId={removeContactHandler}/>
+    </div>
+  );
+}
+
+export default App;
